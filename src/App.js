6428eaeedcf7531a9bae90bd29b45fcdcc39004d
venbarc <118934661+venbarc@users.noreply.github.com>
2024-02-25 23:43:19 +0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Pages from './pages/Pages';
+import Category from './components/Category';
+import { BrowserRouter } from 'react-router-dom';
+import Search from './components/Search';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-            
+    <div className='App'>
+      <BrowserRouter>
+        <Nav>
+          <Logo to={'/'}>FOODFLIX</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
+
+const Logo =styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center
+
+  svg{
+    font-size: 2rem;
+  }
+`;
 
 export default App;
